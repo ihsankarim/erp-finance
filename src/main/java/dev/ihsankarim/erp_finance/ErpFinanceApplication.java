1a303301c5632edcdd53f22ea7acdc5d50518d89
ihsankarim <ihsanalkarim305@gmail.com>
2025-05-25 07:33:41 +0700

    initialize project erp finance management

---
+++ src/main/java/dev/ihsankarim/erp_finance/ErpFinanceApplication.java
@@ -0,0 +1,13 @@
+package dev.ihsankarim.erp_finance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErpFinanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErpFinanceApplication.class, args);
+	}
+
+}
